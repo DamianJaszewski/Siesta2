1d86b0a2ce46e9c63b54fe67d5cac494f80b146c
DamianJaszewski <DamianJaszewski@users.noreply.github.com>
2020-07-28 14:57:07 +0200

    Adding Recipe Class

---
+++ Siesta2/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siesta2
+{
+    public class Recipe
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int PreepTime { get; set; }
+        public string Instruction { get; set; }
+    }   
+}
