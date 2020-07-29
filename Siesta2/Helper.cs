174a08e75ca5a4b7cd63e0ee099f61561636c629
DamianJaszewski <DamianJaszewski@users.noreply.github.com>
2020-07-29 13:25:00 +0200

    adding connectionString

---
+++ Siesta2/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siesta2
+{
+    public static class Helper
+    {
+        public static string CnnVal(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+            //www.connectionstrings.com/sql-server/
+        }
+    }
+}
